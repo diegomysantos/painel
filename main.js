a18ae9d6c7ce4ea9a867d1f15288d359db1f4e49
diegomysantos <72887656+diegomysantos@users.noreply.github.com>
2022-10-08 10:15:54 -0300

    subindo arquivos

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    
+    
+
+    const idAudio = `#som_${instrumento}`
+    //console.log(idAudio)
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    //console.log(listaDeTeclas[i])
+}
+
